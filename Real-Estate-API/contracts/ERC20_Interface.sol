b8eb5c9ce0a35ed24829232945c6df3375a77acc
ThinhHuynh <minhthinhlss@gmail.com>
2020-02-01 16:31:51 +0700

    Update & Config WebStorm Workspace

--- Real-Estate-API/contracts/ERC20_Interface.sol
+++ Real-Estate-API/contracts/ERC20_Interface.sol
@@ -16,5 +16,6 @@ contract ERC20 {
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
     event Transfer(address indexed from, address indexed to, uint tokens);
+
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
